cdc4e0d8c65af2c66f2172173918c61dbe81d549
AlejandraCaicedo <caicedomaria905@gmail.com>
2021-10-25 17:53:10 -0500

    Adding new changes and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, authentication_keys: [:username]
+
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+
   has_many :comments, dependent: :destroy
   has_many :articles, dependent: :destroy
-
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-  root "articles#index"
+  devise_for :users
 
+  root "articles#index"
+  
+  resources :users
   resources :articles do
     resources :comments
   end
+  
 end
