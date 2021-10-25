5897f387928c079e3e7c1dec5030e724b3ba3213
AlejandraCaicedo <caicedomaria905@gmail.com>
2021-10-25 16:54:16 -0500

    Adding has many to articles and users

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
   include Visible
 
   has_many :comments, dependent: :destroy
+  has_many :users
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
