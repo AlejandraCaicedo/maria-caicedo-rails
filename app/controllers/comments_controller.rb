4fd2ff312afd88a449e007fccaed325b3141651a
AlejandraCaicedo <caicedomaria905@gmail.com>
2021-11-05 10:23:25 -0500

    Adding a short validation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,32 +4,22 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.new(comment_params)
-    @comment.commenter = current_user.username
-    if @comment.save
-      redirect_to article_path(@article)
-    else
-      render :root_path
-    end
-  end
-
-  def create
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     current_user.comments << @comment
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-    @comment.destroy
-    redirect_to article_path(@article)
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.find(params[:id])
+      @comment.destroy
+      redirect_to article_path(@article)
   end
 
   private
-  
+
   def comment_params
-    params.require(:comment).permit(:commenter, :body, :status)
+      params.require(:comment).permit(:body, :status)
   end
+  
 end
